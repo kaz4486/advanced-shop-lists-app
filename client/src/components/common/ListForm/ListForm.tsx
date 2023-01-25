1885d67948b3a6e54f0a5ef0dc2808d4d18d5f67
Kamil <kaz4486@gmail.com>
2023-01-25 16:28:03 +0100

    Add useFieldArray to ListForm

--- client/src/components/common/ListForm/ListForm.tsx
+++ client/src/components/common/ListForm/ListForm.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useForm, useFieldArray, Control } from 'react-hook-form';
+import { useForm, useFieldArray, useWatch, Controller } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { addItem } from '../../../redux/itemsRedux';
 
@@ -7,7 +7,7 @@ const ListForm = ({ system }) => {
   const dispatch = useDispatch();
 
   type FormValues = {
-    item: { name: string; amount: number; unit: string }[];
+    item: { name: string; amount: number; unit: string; volume: number }[];
   };
   const {
     register,
@@ -17,11 +17,11 @@ const ListForm = ({ system }) => {
     control,
   } = useForm<FormValues>({
     defaultValues: {
-      item: [{ name: '', amount: 1, unit: '' }],
+      item: [{ name: '', amount: 1, unit: '', volume: 0 }],
     },
   });
 
-  const { fields } = useFieldArray({
+  const { fields, append, prepend, remove, update } = useFieldArray({
     name: 'item',
     control,
   });
@@ -29,14 +29,56 @@ const ListForm = ({ system }) => {
   useEffect(() => {
     if (isSubmitSuccessful) {
       reset({
-        item: [{ name: '', amount: 1, unit: '' }],
+        item: [{ name: '', amount: 1, unit: '', volume: 0 }],
       });
     }
   }, [isSubmitSuccessful, reset]);
 
   const onSubmit = ({ ...data }: FormValues) => {
-    console.log(data.item);
     data.item.forEach((element) => dispatch(addItem({ ...element })));
+  };
+
+  const ConditionallInput = ({ control, index, field }) => {
+    const value = useWatch({
+      name: 'item',
+      control,
+    });
+
+    return (
+      <Controller
+        control={control}
+        name={`item.${index}.volume`}
+        render={({ field }) =>
+          value?.[index]?.unit !== '' ? (
+            <input
+              type='number'
+              min='0'
+              {...register(`item.${index}.volume`, {
+                disabled: false,
+                valueAsNumber: true,
+                min: {
+                  value: 1,
+                  message: 'There must be at least one product of this kind',
+                },
+              })}
+            />
+          ) : (
+            <input
+              type='number'
+              min='0'
+              {...register(`item.${index}.volume`, {
+                disabled: true,
+                valueAsNumber: true,
+                min: {
+                  value: 1,
+                  message: 'There must be at least one product of this kind',
+                },
+              })}
+            />
+          )
+        }
+      ></Controller>
+    );
   };
 
   return (
@@ -56,6 +98,7 @@ const ListForm = ({ system }) => {
                 })}
                 placeholder='name'
               />
+              <p>{errors.item?.[index]?.name?.message}</p>
             </label>
 
             <label>
@@ -64,14 +107,16 @@ const ListForm = ({ system }) => {
                 type='number'
                 min='1'
                 {...register(`item.${index}.amount`, {
-                  valueAsNumber: true,
-                  min: {
-                    value: 1,
-                    message: 'There must be at least one product of this kind',
+                  required: {
+                    value: true,
+                    message: 'Amount is required',
                   },
+                  valueAsNumber: true,
+                  min: 1,
                 })}
                 placeholder='amount'
               />
+              <p>{errors.item?.[index]?.amount?.message}</p>
             </label>
             <label>
               <span>Unit</span>
@@ -97,99 +142,56 @@ const ListForm = ({ system }) => {
                 </select>
               )}
             </label>
+            <label>
+              <span>volume</span>
+              <ConditionallInput
+                key={field.id}
+                {...{ control, index, field }}
+              />
+            </label>
+            <button type='button' onClick={() => remove(index)}>
+              Remove
+            </button>
+            <button
+              type='button'
+              onClick={() =>
+                update(index, { name: '', amount: 1, unit: '', volume: 0 })
+              }
+            >
+              Reset
+            </button>
           </section>
         );
       })}
-
-      {/* <input
-        type='string'
-        {...register('name', {
-          required: {
-            value: true,
-            message: 'Name is required',
-          },
-        })}
-        placeholder='name'
-      />
-      <p>{errors.name?.message}</p>
-      <input
-        type='number'
-        min='1'
-        {...register('amount', {
-          valueAsNumber: true,
-          min: {
-            value: 1,
-            message: 'There must be at least one product of this kind',
-          },
-        })}
-        placeholder='amount'
-      />
-      <p>{errors.amount?.message}</p>
-      {system === 'metric' && (
-        <select {...register('unit')}>
-          <option value=''>Select...</option>
-          <option value='millilitre'>millilitre</option>
-          <option value='litre'>litre</option>
-          <option value='milligram'>milligram</option>
-          <option value='gram'>gram</option>
-          <option value='kilogram'>kilogram</option>
-        </select>
-      )}
-      {system === 'imperial' && (
-        <select {...register('unit')}>
-          <option value=''>Select...</option>
-          <option value='fluid ounce'>fluid once</option>
-          <option value='pint'>pint</option>
-          <option value='gallon'>gallon</option>
-          <option value='ounce'>ounce</option>
-          <option value='pound'>pound</option>
-          <option value='stone'>stone</option>
-        </select>
-      )} */}
-
+      <button
+        type='button'
+        onClick={() => {
+          append({
+            name: '',
+            amount: 1,
+            unit: '',
+            volume: 0,
+          });
+        }}
+      >
+        Append
+      </button>
+      <button
+        type='button'
+        onClick={() => {
+          prepend({
+            name: '',
+            amount: 1,
+            unit: '',
+            volume: 0,
+          });
+        }}
+      >
+        Prepend
+      </button>
       <button type='submit'>Create</button>
-      <button type='reset'>Reset</button>
     </form>
   );
-
-  //   const [name, setName] = useState('');
-  //   const [amount, setAmount] = useState('');
-  //   // const [amount, setAmount] = useState('')
-  //   // const [amount, setAmount] = useState('')
-
-  //   const dispatch = useDispatch();
-
-  //   const handleAddItem = (e) => {
-  //     e.preventDefault();
-  //     dispatch(addItem({ name, amount }));
-  //     setName('');
-  //     setAmount('');
-  //   };
-
-  //   const handleClearItem = (e) => {
-  //     e.preventDefault();
-  //     setName('');
-  //     setAmount('');
-  //   };
-  //   return (
-  //     <form onSubmit={handleAddItem}>
-  //       <input
-  //         type='text'
-  //         value={name}
-  //         onChange={(e) => setName(e.target.value)}
-  //       />
-  //       <input
-  //         type='text'
-  //         value={amount}
-  //         onChange={(e) => setAmount(e.target.value)}
-  //       />
-
-  //       <button type='submit'>Add item</button>
-  //       <button type='reset' onClick={handleClearItem}>
-  //         Clear item
-  //       </button>
-  //     </form>
-  //   );
 };
 
 export default ListForm;
